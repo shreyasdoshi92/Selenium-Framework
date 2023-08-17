3df114d31dc2b501d5a211f585053f144109be0b
shreyas <shreyasdoshi48@gmail.com>
2023-08-17 17:35:54 +0530

    second commit

---
+++ src/test/java/Automation/SeleniumrepeatProjects/StepDefinations/SubmitOrderStepDefination.java
@@ -0,0 +1,60 @@
+package Automation.SeleniumrepeatProjects.StepDefinations;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+
+import Automation.SeleniumrepeatProject.PageObjects.CartPage;
+import Automation.SeleniumrepeatProject.PageObjects.CheckOutPage;
+import Automation.SeleniumrepeatProject.PageObjects.ConfrmationPage;
+import Automation.SeleniumrepeatProject.PageObjects.LandingPage;
+import Automation.SeleniumrepeatProject.PageObjects.ProductCatalogue;
+import Automation.SeleniumrepeatProjects.TestComponents.BaseTest;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SubmitOrderStepDefination extends BaseTest {
+	
+	public LandingPage landingPage;
+	public ProductCatalogue productcatalogue;
+	public CartPage cartpage;
+	public ConfrmationPage confirmationPage;
+	
+	@Given("I landed on the ecommerce page")
+	public void i_landed_on_the_ecommerce_page() throws IOException {
+		landingPage = launchApplication();
+	}
+
+	@Given("^Logged in with username (.+) and password (.+)$")
+	public void logged_in_with_username_and_password(String username, String password) {
+		productcatalogue = landingPage.loginApplication(username, password);
+	   
+	}
+
+	@When("^I add the product (.+) to the cart$")
+	public void i_add_the_product_to_the_cart(String ProductName) {
+		productcatalogue.addProductToCart(ProductName);
+	   
+	}
+
+	@When("^checkout (.+) and select the country (.+) and submit the order$")
+	public void checkout_productname_and_select_the_country_and_submit_the_order(String ProductName, String country) throws InterruptedException {
+		cartpage = productcatalogue.goToCartPage();
+		Boolean match = cartpage.verifyProductDispaly(ProductName);
+		Assert.assertTrue(match);
+
+		CheckOutPage checkOutPage = cartpage.goToCheckout();
+		checkOutPage.selectCountry(country);
+
+		confirmationPage = checkOutPage.submitOrder();
+	}
+
+	@Then("{string} message is displayed on confirmation page")
+	public void message_is_displayed_on_confirmation_page(String msg) {
+		String FinalSuccessMsg = confirmationPage.confirmationMsg();
+		Assert.assertTrue(FinalSuccessMsg.equalsIgnoreCase(msg));
+	
+	}
+
+}
---
+++ target/test-classes/Cucumber/SubmitOrder.feature
@@ -0,0 +1,17 @@
+@tag
+Feature: Purchase the order from Ecommerce website
+
+Background: 
+Given I landed on the ecommerce page
+
+Scenario Outline: Positive Test of submitting the order
+Given Logged in with username <username> and password <password>
+When I add the product <ProductName> to the cart
+And checkout <Productname> and select the country <Country> and submit the order
+Then "THANKYOU FOR THE ORDER." message is displayed on confirmation page
+
+Examples:
+|username					|password		|ProductName		|Country		|
+|shreyasdoshi48@gmail.com	|Mkyasv@1234	|ZARA COAT 3		|india			|
+|sswarali05@gmail.com		|Ssd@12345		|ADIDAS ORIGINAL	|aus			|
+
